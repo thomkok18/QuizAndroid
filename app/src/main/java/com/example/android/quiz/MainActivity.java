30687d3a5b10129004b4cdaf1fb9f91b3c9ad7eb
Thom Kok <tkok@roc-dev.com>
2017-02-14 14:58:04 +0100

    update

--- app/src/main/java/com/example/android/quiz/MainActivity.java
+++ app/src/main/java/com/example/android/quiz/MainActivity.java
@@ -7,17 +7,19 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText answer;
+    EditText answer1, answer2, answer3;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        answer = (EditText) findViewById(R.id.answerText);
+        answer1 = (EditText) findViewById(R.id.answerText1);
+        answer2 = (EditText) findViewById(R.id.answerText2);
+        answer3 = (EditText) findViewById(R.id.answerText3);
     }
 
     public void answerButton1(View view) {
-        String answer1 = answer.getText().toString();
-        if (answer1.matches("Nederland")) {
+        String answer = answer1.getText().toString();
+        if (answer.matches("Nederland")) {
             Toast.makeText(getApplicationContext(), "The first answer is correct!", Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(getApplicationContext(), "The first answer is incorrect!", Toast.LENGTH_SHORT).show();
@@ -25,8 +27,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void answerButton2(View view) {
-        String answer1 = answer.getText().toString();
-        if (answer1.matches("Nederland")) {
+        String answer = answer2.getText().toString();
+        if (answer.matches("Nederland")) {
             Toast.makeText(getApplicationContext(), "The first answer is correct!", Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(getApplicationContext(), "The first answer is incorrect!", Toast.LENGTH_SHORT).show();
@@ -34,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void answerButton3(View view) {
-        String answer1 = answer.getText().toString();
-        if (answer1.matches("Nederland")) {
+        String answer = answer3.getText().toString();
+        if (answer.matches("Nederland")) {
             Toast.makeText(getApplicationContext(), "The first answer is correct!", Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(getApplicationContext(), "The first answer is incorrect!", Toast.LENGTH_SHORT).show();
