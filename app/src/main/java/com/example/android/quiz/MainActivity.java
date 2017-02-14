c94c8fcc6d91d21beba4b6b1ffbc3cc8832765e2
Thom Kok <tkok@roc-dev.com>
2017-02-14 20:02:16 +0100

    update

--- app/src/main/java/com/example/android/quiz/MainActivity.java
+++ app/src/main/java/com/example/android/quiz/MainActivity.java
@@ -3,23 +3,37 @@ package com.example.android.quiz;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText answer1, answer2, answer3;
+    EditText answer1;
+    CheckBox yesCheckbox, noCheckbox;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         answer1 = (EditText) findViewById(R.id.answerText1);
-        answer2 = (EditText) findViewById(R.id.answerText2);
-        answer3 = (EditText) findViewById(R.id.answerText3);
+        yesCheckbox = (CheckBox) findViewById(R.id.yes);
+        noCheckbox = (CheckBox) findViewById(R.id.no);
+
+    }
+
+    public void checkboxYes(View view) {
+        if (yesCheckbox.isChecked()) {
+            noCheckbox.setChecked(false);
+        }
+    }
+    public void checkboxNo(View view) {
+        if (noCheckbox.isChecked()) {
+            yesCheckbox.setChecked(false);
+        }
     }
 
     public void answerButton1(View view) {
         String answer = answer1.getText().toString();
-        if (answer.matches("Nederland")) {
+        if (answer.matches("Monday")) {
             Toast.makeText(getApplicationContext(), "The first answer is correct!", Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(getApplicationContext(), "The first answer is incorrect!", Toast.LENGTH_SHORT).show();
@@ -27,20 +41,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void answerButton2(View view) {
-        String answer = answer2.getText().toString();
-        if (answer.matches("Nederland")) {
-            Toast.makeText(getApplicationContext(), "The first answer is correct!", Toast.LENGTH_SHORT).show();
+        if (yesCheckbox.isChecked()) {
+            Toast.makeText(getApplicationContext(), "The second answer is correct!", Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(getApplicationContext(), "The first answer is incorrect!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "The second answer is incorrect!", Toast.LENGTH_SHORT).show();
         }
     }
 
     public void answerButton3(View view) {
-        String answer = answer3.getText().toString();
-        if (answer.matches("Nederland")) {
-            Toast.makeText(getApplicationContext(), "The first answer is correct!", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(getApplicationContext(), "The first answer is incorrect!", Toast.LENGTH_SHORT).show();
-        }
+
     }
 }
