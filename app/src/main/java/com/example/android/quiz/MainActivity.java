fcddf1c94c29358ff5286d9f6b776eee4c9cd051
Thom Kok <tkok@roc-dev.com>
2017-02-14 14:40:07 +0100

    update

--- app/src/main/java/com/example/android/quiz/MainActivity.java
+++ app/src/main/java/com/example/android/quiz/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.android.quiz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText answer;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        answer = (EditText) findViewById(R.id.answerText);
+    }
+
+    public void answerButton(View view) {
+        String answer1 = answer.getText().toString();
+        if (answer1.matches("Nederland")) {
+            Toast.makeText(getApplicationContext(), "The first answer is correct!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "The first answer is incorrect!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
